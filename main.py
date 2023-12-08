486f10b4857ae78c18ed4ad7fb29567c0d26039b
jeremiah.1582@googlemail.com <jeremiah.1582@googlemail.com>
2023-12-08 13:14:52 +0100

    updated code

--- main.py
+++ main.py
@@ -3,23 +3,23 @@ import pandas
 import re 
 import os
 
-dataPath = 'file_generator.py/data.txt'
+dataPath = './data.txt'
 allExercises=[]
 
 def formatData(path):
     try:
         os.path.exists(path)
-        with open(path) as f: 
+        with open(path, 'r+') as f: 
+            print(f) # class _io.TextIOWrapper
             # convert file to string 
             # use regex to locate content & save to variables
             content = f.read()
             block = re.split('\n\n', content)
             
-            # print('...block...',(block))
             for text in block:
                 
                 link= re.findall(r'(https:\/\/classroom\.github\.com\/a\/[^\n]+\n*)+', text)
-                # print(link)
+                
                 name= re.search(r'[A-Z][a-z]+(?:\s[A-Z][a-z]+)*(?=\n)',text).group()
                 
                 assignment_name= re.findall(r'(?:Assignment|Topic):\s*(.*?)(?=https|:white_check_mark:)', text)       
@@ -37,6 +37,7 @@ def formatData(path):
                     'assignment_name': assignment_name,
                     'links_to_exercise': {f'link{i+1}':val for i,val in enumerate(link)}
                 }
+                
                 allExercises.append(dataCard)
                 
     except AttributeError: 
